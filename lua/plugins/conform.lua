1327f5936e84857b3643d189b18ad12809584232
SHNZHN-01 <shnzhn@protonmail.com>
2026-06-15 17:53:28 -0300

    changes

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,25 +1,22 @@
 require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		nix = { "nixfmt" },
-		-- go              = { "gofumpt" },
-		-- rust            = { "rustfmt" },
-		-- python          = { "ruff_format" },   -- conform builtin, distinct from the linter
-		-- javascript      = { "prettierd" },
-		-- javascriptreact = { "prettierd" },
-		-- typescript      = { "prettierd" },
-		-- typescriptreact = { "prettierd" },
-		-- json            = { "prettierd" },
-		-- jsonc           = { "prettierd" },
-		-- yaml            = { "prettierd" },
-		-- markdown        = { "prettierd" },
-		-- css             = { "prettierd" },
-		-- html            = { "prettierd" },
-		-- sh              = { "shfmt" },
-		-- bash            = { "shfmt" },
-	},
+    formatters_by_ft = {
+        lua = { "stylua" },
+        nix = { "nixfmt" },
+    },
 
-	-- Uncomment for format-on-write. lsp_format = "fallback" applies the same
-	-- rule as the gf mapping: external formatter where configured, else the LSP.
-	-- format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+
+    format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+    end,
 })
+
+vim.api.nvim_create_user_command("FormatToggle", function(args)
+    if args.bang then
+        vim.b.disable_autoformat = not vim.b.disable_autoformat -- :FormatToggle! = this buffer
+    else
+        vim.g.disable_autoformat = not vim.g.disable_autoformat -- :FormatToggle  = global
+    end
+end, { bang = true })
